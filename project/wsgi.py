df9fdb39f78cd001b6f420d7c54c64886b378483
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-05-22 16:12:53 +0200

    Configure WhiteNoise to serve static files

--- project/wsgi.py
+++ project/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(get_wsgi_application())
