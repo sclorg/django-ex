ab4b961cb51e1e2322dc59af09166d679ddd23d3
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-05-24 13:56:43 +0200

    Show server hostname

--- openshift/views.py
+++ openshift/views.py
@@ -1,6 +1,7 @@
+import os
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'openshift/index.html')
+    return render(request, 'openshift/index.html', {'HOSTNAME': os.getenv('HOSTNAME', 'unknown')})
