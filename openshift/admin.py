97b5332be40fd1b1dc92a94f7a0aa1bc6614e6a3
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-05-25 22:32:27 +0200

    Include page view in the admin interface

--- openshift/admin.py
+++ openshift/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import PageView
+
 # Register your models here.
+
+
+class PageViewAdmin(admin.ModelAdmin):
+    list_display = ['hostname', 'timestamp']
+
+admin.site.register(PageView, PageViewAdmin)
