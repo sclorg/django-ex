d65b4c71f15d04115d2718cae5bee154b2d7d3db
Mike Olund <michaelo@openroad.ca>
2017-02-19 17:16:21 -0800

    Created a fake BCEID login screen for use in localdev environments

---
+++ edivorce/apps/core/middleware/bceid_middleware.py
@@ -0,0 +1,47 @@
+import uuid
+
+
+class BceidUser(object):
+    def __init__(self, guid, first_name, last_name, type, is_authenticated):
+        self.guid = guid
+        self.first_name = first_name
+        self.last_name = last_name
+        self.type = type
+        self.is_authenticated = is_authenticated
+
+
+class BceidMiddleware(object):
+    def process_request(self, request):
+        # todo: Make sure the request is coming from the justice proxy (via IP/host check)
+
+        # 1. Real BCeID user
+
+        # todo: parse the siteminder headers and stick them into a dictionary request.bceid_user
+
+        if request.session.get('fake-bceid-guid', False):
+
+            # 2. Fake BCeID user
+            request.bceid_user = BceidUser(
+                guid=request.session.get('fake-bceid-guid', ''),
+                is_authenticated=True,
+                type='FAKE',
+                first_name='Kelly',
+                last_name='Bundy'
+            )
+        else:
+
+            # 3.  Anonymous User
+
+            if request.session.get('anon-guid', False):
+                request.session['anon-guid'] = uuid.uuid4().urn[9:]
+
+            request.bceid_user = BceidUser(
+                guid=request.session.get('anon-guid'),
+                is_authenticated=False,
+                type='ANONYMOUS',
+                first_name='',
+                last_name=''
+            )
+
+    def process_response(self, request, response):
+        return response
---
+++ edivorce/apps/core/views/localdev.py
@@ -0,0 +1,25 @@
+import uuid
+import binascii
+from encodings.utf_8 import decode
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def bceid(request):
+    """ fake bceid login for developer workstation environment """
+    if request.method == "POST":
+        login_name = request.POST.get('user', '')
+
+        # convert the login name to a guid
+        hex_name = decode(binascii.hexlify(str.encode(login_name)))[0]
+        fake_guid = uuid.UUID(hex_name.rjust(32, '0')).urn[9:]
+
+        # save the guid in a session variable
+        request.session['fake-bceid-guid'] = fake_guid
+
+        return redirect(settings.FORCE_SCRIPT_NAME + '/login')
+
+    else:
+        return render(request, 'localdev/bceid.html')
