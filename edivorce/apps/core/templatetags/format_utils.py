4a2ef5e4a662fa65a02d1738ee5bff0b1c521125
Mike Olund <michaelo@openroad.ca>
2017-03-21 18:10:28 -0700

    DIV-354 - Change carriage returns to bullet lists on display for Spousal Support, Property and Debt and Other Orders

---
+++ edivorce/apps/core/templatetags/format_utils.py
@@ -0,0 +1,14 @@
+from django import template
+import re
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter
+def linebreaksul(value):
+    "Converts strings with newlines into <ul><li></li></ul>s"
+    value = re.sub(r'\r\n|\r|\n', '\n', value.strip())  # normalize newlines
+    lines = re.split('\n', value)
+    lines = ['<li>%s</li>' % line for line in lines]
+    return mark_safe('<ul>%s</ul>' % '\n'.join(lines))
