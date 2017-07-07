a46dea03c9ebfdf7cb947d7367d59ea33ea63e08
Charles Shin <charles@openroad.ca>
2017-07-07 16:25:00 -0700

    DIV-478: Updated PDF date format to be dd/mmm/yyyy

--- edivorce/apps/core/templatetags/format_utils.py
+++ edivorce/apps/core/templatetags/format_utils.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import datetime
 import re
 from django.utils.safestring import mark_safe
 
@@ -12,3 +13,13 @@ def linebreaksul(value):
     lines = re.split('\n', value)
     lines = ['<li>%s</li>' % line for line in lines if line and not line.isspace()]
     return mark_safe('<ul>%s</ul>' % '\n'.join(lines))
+
+
+@register.filter
+def date_formatter(value):
+    """
+        Changes date format from dd/mm/yyyy to dd/mmm/yyyy
+    """
+    d = datetime.strptime(value, '%d/%m/%Y')
+    return d.strftime('%d %b %Y')
+
