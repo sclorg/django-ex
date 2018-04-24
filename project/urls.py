79a9f7f552d9979777cc0dcee5c7f44dbe86e10d
jpic <jamespic@gmail.com>
2018-04-24 20:31:11 +0200

    URLs: Django 2.0 style

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 from welcome.views import index, health
@@ -9,13 +9,13 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    path('/', index),
+    path('health', health),
+    path('admin/', include(admin.site.urls)),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
