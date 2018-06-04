5c0b44fbc25b549d1057302735dc8d66d705cc72
Wade Barnes <wade.barnes@shaw.ca>
2018-06-04 14:42:32 -0700

    EDIVORCE-45 - Set label based on environment.

--- edivorce/apps/core/context_processors.py
+++ edivorce/apps/core/context_processors.py
@@ -6,5 +6,6 @@ def settings_processor(request):
     return {
         'gtm_id': settings.GTM_ID,
         'proxy_root_path': settings.FORCE_SCRIPT_NAME,
+        'deployment_environment': settings.ENVIRONMENT,
         'show_debug': settings.ENVIRONMENT in ['localdev', 'dev', 'test', 'minishift']
     }
