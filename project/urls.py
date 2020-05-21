d13d62ecd7da534f5524f27bd0a6fd02ba06cc66
Lumir Balhar <lbalhar@redhat.com>
2020-05-21 16:42:31 +0200

    Fix project settings

--- project/urls.py
+++ project/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from welcome.views import index, health
 
@@ -27,5 +28,5 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
