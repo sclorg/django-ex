9d4e039d06863ba20f4e5cf51fa03a1d24339824
Michael Olund <michaelo@openroad.ca>
2020-09-15 13:58:30 -0700

    Fixing broken migrations

--- manage.py
+++ manage.py
@@ -12,9 +12,13 @@ if __name__ == "__main__":
     else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edivorce.settings.openshift")
 
-        # run npm_build (custom management command) when collectstatic is called in 
+        # run 'npm_build' (custom management command) when collectstatic is called in 
         # the S2I assemble script
         if sys.argv[1] == 'collectstatic':
             execute_from_command_line(['manage.py','npm_build'])
+
+        # run 'loaddata' before running the migrations
+        if sys.argv[1] == 'migrate':
+            execute_from_command_line(['manage.py', 'loaddata', '/opt/app-root/src/edivorce/fixtures/Question.json'])
 
     execute_from_command_line(sys.argv)
