4680b5ed41cc1b06a632a541accfc545e0d53a71
rh-ee-symartin <symartin@redhat.com>
2022-05-18 15:43:56 +0000

    Init test project.

--- project/settings.py
+++ project/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'welcome',
 ]
 
 MIDDLEWARE = [
---
+++ test/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestConfig(AppConfig):
+    name = 'test'
